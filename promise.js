e05515d688376f9d4c07430cdcd8fc2424320666
ghalda <gputribalqis@gmail.com>
2022-09-21 01:52:10 +0700

    challenge promise ghalda

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (feeling) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  //combine theater ixx and vgc
+  allTheater = theaterIXX.concat(theaterVGC);
+
+  //read all result from theater
+  const result = allTheater.filter((review) => review['hasil'] === feeling);
+
+  //to count all result feeling of the film from ixx and vgc
+  return result.length;
+};
 
 module.exports = {
   promiseOutput,
